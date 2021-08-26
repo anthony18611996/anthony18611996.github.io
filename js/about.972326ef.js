c5585d4b3cd3fd666051e5eb0e4a88db725e42b6
anthony1861996 <vaa1861@gmail.com>
2021-08-26 04:49:36 +0300

    rebuild
    rebuild

--- js/about.972326ef.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{a1d1:function(n,t,a){"use strict";a.r(t);var e=a("7a23"),c={class:"about"},o=Object(e["e"])("h1",null,"This is an about page",-1);function s(n,t){return Object(e["n"])(),Object(e["c"])("div",c,[o])}const u={};u.render=s;t["default"]=u}}]);
-//# sourceMappingURL=about.972326ef.js.map
